2ca0ee6c0dba0a982ad7f1adbe2bee6b511786b2
SilviVinkman <32923802+SilviVinkman@users.noreply.github.com>
2018-04-04 19:45:03 +0300

    Initial commit

---
+++ Ylesanne3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ylesanne3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
